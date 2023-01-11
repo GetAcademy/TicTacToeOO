795a68fd1b952d93e59740d82f3c90602fd820d9
GET Academy <terje@getacademy.no>
2023-01-11 14:48:34 +0100

    Initial commit

---
+++ TicTacToeOO/Program.cs
@@ -0,0 +1,14 @@
+﻿using TicTacToeOO;
+
+var board = new Board();
+var gameConsole = new GameConsole(board);
+while (true)
+{
+    gameConsole.Show();
+    Console.Write("Skriv inn hvor du vil sette kryss (f.eks. \"a2\"): ");
+    var position = Console.ReadLine();
+    board.Mark(position);
+    gameConsole.Show();
+    Thread.Sleep(2000);
+    board.MarkRandom(false);
+}
